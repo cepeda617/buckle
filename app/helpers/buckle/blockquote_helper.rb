e70e58159bfa676194ac63c67c29d44516605ea9
Peter Cepeda <pcepeda@pubget.com>
2013-12-04 17:29:08 -0500

    Adding paragraph tag to blockquote_tag output

--- app/helpers/buckle/blockquote_helper.rb
+++ app/helpers/buckle/blockquote_helper.rb
@@ -3,6 +3,7 @@ module Buckle
 
     def blockquote_tag( content, options = {} )
       citation = '<small>%s</small>' % options[:cite] if options[:cite].present?
+      content = content_tag :p, content
       content_tag :blockquote, [content, citation].join.html_safe, options.slice!(:cite)
     end
 
