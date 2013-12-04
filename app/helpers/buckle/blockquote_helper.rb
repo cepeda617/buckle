bb33de474f8eb4e747355e8d68fe610e1918bf29
Peter Cepeda <pcepeda@pubget.com>
2013-12-04 17:30:02 -0500

    Using content_tag helper to render small tag in blockquote_tag

--- app/helpers/buckle/blockquote_helper.rb
+++ app/helpers/buckle/blockquote_helper.rb
@@ -2,7 +2,7 @@ module Buckle
   module BlockquoteHelper
 
     def blockquote_tag( content, options = {} )
-      citation = '<small>%s</small>' % options[:cite] if options[:cite].present?
+      citation = content_tag :small, options[:cite] if options[:cite].present?
       content = content_tag :p, content
       content_tag :blockquote, [content, citation].join.html_safe, options.slice!(:cite)
     end
