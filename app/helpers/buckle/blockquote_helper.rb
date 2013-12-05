ed8f64884dc6d4786bac0f125fa38779c7aa2c8a
Peter Cepeda <pcepeda@pubget.com>
2013-12-05 09:07:46 -0500

    Allowing block content in blockquote helper

--- app/helpers/buckle/blockquote_helper.rb
+++ app/helpers/buckle/blockquote_helper.rb
@@ -2,9 +2,10 @@ module Buckle
   module BlockquoteHelper
 
     def blockquote_tag( content, options = {} )
-      citation = content_tag :small, options[:cite] if options[:cite].present?
-      content = content_tag :p, content
-      content_tag :blockquote, [content, citation].join.html_safe, options.slice!(:cite)
+      content_tag :blockquote, options.slice(:cite) do
+        concat content_tag :p, block_given? ? capture(&block) : content
+        concat content_tag :small, options[:cite] if options.key? :cite
+      end
     end
 
   end
