470c5dc3fb338e001a5670bfa01554c6db0e810c
Peter Cepeda <pcepeda@pubget.com>
2013-12-05 10:57:58 -0500

    Tweaking panel helpers to allow for block content

--- app/helpers/buckle/panel_helper.rb
+++ app/helpers/buckle/panel_helper.rb
@@ -3,14 +3,18 @@ module Buckle
 
     def panel_tag( type = :default, content = nil, options = {}, &block )
       content_tag :div, class: "panel panel-#{ type.to_s }" do
-        panel_heading(options[:header]) if options.key? :header
-        content_tag :div, (concat block_given? ? capture(&block) : content), class: 'panel-body'
-        panel_footer(options[:footer]) if options.key? :footer
+        concat panel_heading(options[:header]) if options.key? :header
+        concat block_given? ? capture(&block) : panel_body(content)
+        concat panel_footer(options[:footer]) if options.key? :footer
       end
     end
 
     def panel_heading( content )
       content_tag :div, panel_title(content), class: 'panel-heading'
+    end
+
+    def panel_body( content = nil, &block )
+      content_tag :div, content, class: 'panel-body', &block
     end
 
     def panel_footer( content )
